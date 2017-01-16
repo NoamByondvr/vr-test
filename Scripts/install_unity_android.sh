9bd88bd1dcf98cad14213aa5965c650006115a27
NoamByondvr <noamiko@gmail.com>
2017-01-17 00:44:27 +0200

    121212

--- Scripts/install_unity_android.sh
+++ Scripts/install_unity_android.sh
@@ -5,7 +5,7 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo '## Downloading from http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg: '
-curl -o $(pwd)/Unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
+curl -o Unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
 
 echo '## Installing Unity.pkg'
-sudo installer -package $(pwd)/Unity.pkg -target /
+sudo installer -package Unity.pkg -target /
