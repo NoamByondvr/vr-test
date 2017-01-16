8e705c766842972ab838b31802e0d97d76181372
noamiko <noamiko@gmail.com>
2017-01-16 19:51:46 +0200

    first commit

---
+++ Assets/Editor/Build.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+
+public class BuildMyGame {
+
+    public static void BuildAndroid() {
+        Build(BuildTarget.Android);
+    }
+
+    public static void BuildOSX() {
+        Build(BuildTarget.StandaloneOSXIntel64);
+    }
+
+    public static void BuildWindows() {
+        Build(BuildTarget.StandaloneWindows);
+    }
+
+    public static void Build(BuildTarget target) {
+		string[] levels = new string[1]{"Assets/main.unity"};
+        BuildPipeline.BuildPlayer(levels.ToArray(), Environment.GetCommandLineArgs().Last(), target,
+            BuildOptions.None);
+    }
+}
---
+++ Scripts/build.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# Change this the name of your project. This will be the name of the final executables as well.
+project="ci-build"
+
+echo "Attempting to build $project for Android"
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -quit
+  -executeMethod BuildMyGame.BuildAndroid "$(pwd)/Build/windows/$project.apk"
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
