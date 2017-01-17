eb2f44c7c4dec78a08a78910107ed09fe87e0ff8
NoamByondvr <noamiko@gmail.com>
2017-01-17 11:38:16 +0200

    212121212

--- Scripts/install_android_sdk.sh
+++ Scripts/install_android_sdk.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export JAVA_HOME=$(/usr/libexec/java_home)
 
-echo "y" | android update sdk --no-ui --all --filter tool,platform-tool,android-23,android-24,android-25,build-tools-25,build-tools-25.0.1,build-tools-25.0.2
+echo "y" | android update sdk --no-ui --all --filter tool,platform-tool,android-23,android-24,android-25,build-tools-25.0.1,build-tools-25.0.2
 
 # echo "yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null"
 # echo yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null
--- Scripts/install_unity_android.sh
+++ Scripts/install_unity_android.sh
@@ -5,10 +5,10 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
-ls -l
-
 echo '## Downloading Android package from http://download.unity3d.com/download_unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg: '
 curl -o Android.pkg http://download.unity3d.com/download_unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg
+
+ls -l
 
 echo '## Installing Android.pkg'
 sudo installer -package Android.pkg -target /
