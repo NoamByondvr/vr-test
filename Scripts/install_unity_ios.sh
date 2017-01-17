a27e0987eda8064a9ccaf9841e9bef2529c8d67a
NoamByondvr <noamiko@gmail.com>
2017-01-17 16:43:14 +0200

    9999

--- Scripts/install_unity_ios.sh
+++ Scripts/install_unity_ios.sh
@@ -4,7 +4,7 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-mkdir -p ./unity
+mkdir -p $(pwd)/unity
 
 echo '## curl -o unity/unity-ios.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-5.4.4f1.pkg'
 curl -o $(pwd)/unity/unity-ios.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-5.4.4f1.pkg
