f34e3d91227c86b2d15b9d6e33c7fe2efa6c4738
NoamByondvr <noamiko@gmail.com>
2017-01-17 19:38:46 +0200

    tring to cache

--- Scripts/install_android_sdk.sh
+++ Scripts/install_android_sdk.sh
@@ -1,15 +1,28 @@
 #! /bin/sh
-mkdir -p $(pwd)/android-sdk
 
-echo "## curl -o $(pwd)/android-sdk/android-sdk.zip http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip"
-curl -o $(pwd)/android-sdk/android-sdk.zip http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip
+VERSION=r25.2.3
+BASEURL=http://dl.google.com/android/repository
+URL=$BASEURL/tools_$VERSION-macosx.zip
 
-echo "## unzip $(pwd)/android-sdk/android-sdk.zip"
-unzip $(pwd)/android-sdk/android-sdk.zip -d $(pwd)/android-sdk
+DST_ZIP=$HOME/.cache/downloads/android-sdk/android-sdk.zip
+ANDROID_DIR=$HOME/.cache/android
 
-echo "y" | $(pwd)/android-sdk/tools/android update sdk --no-ui --all --filter tool,platform-tool,android-23,android-24,android-25,build-tools-25.0.1,build-tools-25.0.2
+if [ -f "$DST_ZIP" ];
+then
+   echo "File $DST_ZIP exist."
+else
+    echo "File $DST_ZIP does not exist" >&2
+    mkdir -p $HOME/.cache/downloads/android-sdk
+    echo "## curl -o $DST_ZIP $URL"
+    curl -o $DST_ZIP $URL
 
-export ANDROID_HOME=$PWD/android-sdk/android-sdk-macosx
+    echo "## unzip $DST_ZIP -d $ANDROID_DIR"
+    unzip $DST_ZIP -d $ANDROID_DIR
+
+	echo "y" | $ANDROID_DIR/tools/android update sdk --no-ui --all --filter tool,platform-tool,android-23,android-24,android-25,build-tools-25.0.1,build-tools-25.0.2
+fi
+
+export ANDROID_HOME=$ANDROID_DIR/android-sdk-macosx
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export JAVA_HOME=$(/usr/libexec/java_home)
--- Scripts/install_unity.sh
+++ Scripts/install_unity.sh
@@ -4,28 +4,33 @@ BASE_URL=http://download.unity3d.com/download_unity
 HASH=5a3967d8c55d
 VERSION=5.4.4f1
 
+DOWNLOAD_DIR=$HOME/.cache/downloads
+
 mkdir -p $(pwd)/unity
 
 download() {
-    package=$1
+    url=$1
     dst=$2
-    url="$BASE_URL/$HASH/$package"
-
-    echo "## Downloading from $url: "
-    echo "## into $pwd/unity/$dst"
-    curl -o $(pwd)/unity/$(dst) $(url)
+    echo "## Downloading from: $url"
+    echo "## to: $dst"
+    curl -o $(dst) $(url)
 }
 
 install() {
-    package=$1
-    dst=$2
-    download "$package" "$dst"
 
-    echo "Installing $pwd/unity/$dst"
-    sudo installer -dumplog -package $(pwd)/unity/$(dst) -target /
+    package=$1
+    file_name=$2
+
+    if [ -f "$DST_ZIP" ];
+    then
+        echo "File $DST_ZIP exist."
+    else
+        download "$BASE_URL/$HASH/$package" "$DOWNLOAD_DIR/$file_name"
+
+    	echo "Installing $DOWNLOAD_DIR/$file_name"
+    	sudo installer -dumplog -package $(DOWNLOAD_DIR)/$(file_name) -target /
+    fi
 }
 
-install "MacEditorInstaller/Unity-$VERSION.pkg" "unity.pkg"
+install "MacEditorInstaller/Unity-$VERSION.pkg" "$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-$VERSION.pkg" "unity.pkg"
-
-sudo rm -rf $(pwd)/unity
