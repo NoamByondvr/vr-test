6f7630fdeebc92e35ab072094f64b703e170cf3c
NoamByondvr <noamiko@gmail.com>
2017-01-17 16:03:10 +0200

    3333

--- Scripts/install_unity.sh
+++ Scripts/install_unity.sh
@@ -4,8 +4,11 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
+
+mkdir -p ./unity
+
 echo '## Downloading from http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg: '
-curl -o unity/unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
+curl -o ./unity/unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
 
 echo '## Installing Unity.pkg'
-sudo installer -dumplog -package unity/unity.pkg -target /unity
+sudo installer -dumplog -package ./unity/unity.pkg -target ./unity
--- Scripts/install_unity_android.sh
+++ Scripts/install_unity_android.sh
@@ -5,6 +5,8 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 
+mkdir -p ./unity
+
 echo '## Downloading Android package from http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg: '
 curl -o unity/unity-android.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg
 
