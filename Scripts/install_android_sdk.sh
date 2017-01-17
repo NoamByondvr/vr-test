e64f48b57f0e9d363c48435d05c4ea5468daf86b
NoamByondvr <noamiko@gmail.com>
2017-01-17 11:33:45 +0200

    202020020

--- Scripts/install_android_sdk.sh
+++ Scripts/install_android_sdk.sh
@@ -1,36 +1,37 @@
 #! /bin/sh
 
 echo "wget http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip"
-wget http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip
+curl -o android-sdk.zip http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip
 
-echo "unzip tools_r25.2.3-macosx.zip"
-unzip tools_r25.2.3-macosx.zip
+echo "unzip android-sdk.zip"
+unzip android-sdk.zip
 
-# echo "export ANDROID_HOME=$PWD/android-sdk-macosx"
-# export ANDROID_HOME=$PWD/android-sdk-macosx
+export ANDROID_HOME=$PWD/android-sdk-macosx
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export JAVA_HOME=$(/usr/libexec/java_home)
 
-# echo "export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
-# export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
+echo "y" | android update sdk --no-ui --all --filter tool,platform-tool,android-23,android-24,android-25,build-tools-25,build-tools-25.0.1,build-tools-25.0.2
 
-echo "yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null
 
-echo "yes | ./tools/android update sdk --filter android-23 --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter android-23 --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter android-23 --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter android-23 --no-ui --force > /dev/null
 
-echo "yes | ./tools/android update sdk --filter android-24 --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter android-24 --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter android-24 --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter android-24 --no-ui --force > /dev/null
 
-echo "yes | ./tools/android update sdk --filter android-25 --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter android-25 --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter android-25 --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter android-25 --no-ui --force > /dev/null
 
-echo "yes | ./tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null
 
-echo "yes | ./tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null
 
-echo "yes | ./tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null"
-echo yes | ./tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null
+# echo "yes | ./tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null"
+# echo yes | ./tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null
 
-export PATH=${PATH}/tools/platform-tools/build-tools/25.0.2
+# export PATH=${PATH}/tools/platform-tools/build-tools/25.0.2
