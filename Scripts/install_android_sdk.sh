1d534f870fd2cba18cce59b8974c345e8dec9af7
NoamByondvr <noamiko@gmail.com>
2017-01-17 19:59:33 +0200

    another try

--- Scripts/install_android_sdk.sh
+++ Scripts/install_android_sdk.sh
@@ -18,7 +18,8 @@ else
     echo "## unzip $DST_ZIP -d $ANDROID_DIR"
     unzip $DST_ZIP -d $ANDROID_DIR
 
-	echo "y" | $ANDROID_DIR/tools/android update sdk --no-ui --all --filter tool,platform-tool,android-23,android-24,android-25,build-tools-25.0.1,build-tools-25.0.2
+	echo "y" | $ANDROID_DIR/tools/android update sdk --no-ui --all --filter /
+	tool,platform-tool,android-23,android-24,android-25,build-tools-25.0.1,build-tools-25.0.2
 fi
 
 export ANDROID_HOME=$ANDROID_DIR/android-sdk-macosx
