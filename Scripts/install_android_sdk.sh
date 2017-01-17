ee3096daef8346c71ccbc0a629d3535ed7376853
NoamByondvr <noamiko@gmail.com>
2017-01-17 11:16:18 +0200

    17171

--- Scripts/install_android_sdk.sh
+++ Scripts/install_android_sdk.sh
@@ -12,26 +12,25 @@ unzip tools_r25.2.3-macosx.zip
 # echo "export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 # export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter platform-tools --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter platform-tools --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter platform-tools --no-ui --force > /dev/null
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter android-23 --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter android-23 --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter android-23 --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter android-23 --no-ui --force > /dev/null
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter android-24 --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter android-24 --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter android-24 --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter android-24 --no-ui --force > /dev/null
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter android-25 --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter android-25 --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter android-25 --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter android-25 --no-ui --force > /dev/null
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null
 
-echo "yes | ./android-sdk-macosx/tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null"
-echo yes | ./android-sdk-macosx/tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null
+echo "yes | ./tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null"
+echo yes | ./tools/android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null
 
-export ANDROID_HOME=./android-sdk-macosx
-export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/25.0.2
+export PATH=${PATH}/tools/platform-tools/build-tools/25.0.2
