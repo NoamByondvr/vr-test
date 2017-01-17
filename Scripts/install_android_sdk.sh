795b9a3b060496110c1bc2ade99b780e0f46ae3f
NoamByondvr <noamiko@gmail.com>
2017-01-17 10:24:05 +0200

    141414

--- Scripts/install_android_sdk.sh
+++ Scripts/install_android_sdk.sh
@@ -1,13 +1,34 @@
 #! /bin/sh
 
+echo "wget http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip"
 wget http://dl.google.com/android/repository/tools_r25.2.3-macosx.zip
+
+echo "unzip tools_r25.2.3-macosx.zip"
 unzip tools_r25.2.3-macosx.zip
+
+echo "export ANDROID_HOME=$PWD/android-sdk-macosx"
 export ANDROID_HOME=$PWD/android-sdk-macosx
+
+echo "export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
+
+echo "yes | android update sdk --filter platform-tools --no-ui --force > /dev/null"
 echo yes | android update sdk --filter platform-tools --no-ui --force > /dev/null
+
+echo "yes | android update sdk --filter android-23 --no-ui --force > /dev/null"
 echo yes | android update sdk --filter android-23 --no-ui --force > /dev/null
+
+echo "yes | android update sdk --filter android-24 --no-ui --force > /dev/null"
 echo yes | android update sdk --filter android-24 --no-ui --force > /dev/null
+
+echo "yes | android update sdk --filter android-25 --no-ui --force > /dev/null"
 echo yes | android update sdk --filter android-25 --no-ui --force > /dev/null
+
+echo "yes | android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null"
 echo yes | android update sdk --filter build-tools-25 --all --no-ui --force > /dev/null
+
+echo "yes | android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null"
 echo yes | android update sdk --filter build-tools-25.0.1 --all --no-ui --force > /dev/null
+
+echo "yes | android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null"
 echo yes | android update sdk --filter build-tools-25.0.2 --all --no-ui --force > /dev/null
