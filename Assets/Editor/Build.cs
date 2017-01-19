8cc6264a86e553e705b5719a317f0d85f64328f1
NoamByondvr <noamiko@gmail.com>
2017-01-19 08:06:38 +0200

    asdf

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -8,7 +8,7 @@ public class BuildMyGame {
 
     public static void BuildAndroid() {
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel19;
-        PlayerSettings.bundleIdentifier = "com.ccccc.ppppp"
+        PlayerSettings.bundleIdentifier = "com.ccccc.ppppp";
         //EditorSetup.AndroidSdkRoot = Environment.GetEnvironmentVariable("androidSdkPath");
         Build(BuildTarget.Android);
     }
