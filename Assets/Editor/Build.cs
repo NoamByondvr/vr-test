86bdf2aa190f43b1e1d51a86f3547eb45fd0b798
NoamByondvr <noamiko@gmail.com>
2017-01-21 12:42:00 +0200

    fix typo

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -9,7 +9,7 @@ public class BuildMyGame {
     public static void BuildAndroid() {
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel19;
         PlayerSettings.bundleIdentifier = "com.ccccc.ppppp";
-        EditorSetup.AndroidSdkRoot = Environment.GetEnvironmentVariable("androidSdkRoot");
+        EditorSetup.AndroidSdkRoot = Environment.GetEnvironmentVariable("androidSdkPath");
         Build(BuildTarget.Android);
     }
 
